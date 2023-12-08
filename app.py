5e6be27b3a1bd4e60186970d0e28b4b324b8af1c
suyash-03 <suyash.singh9450@gmail.com>
2023-12-08 22:57:26 +0530

    CORS issue fix

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import numpy as np
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 import librosa
+from flask import Flask
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
